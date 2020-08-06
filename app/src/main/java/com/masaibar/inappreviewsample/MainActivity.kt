4fd9bbce2d6a3612001c04871eaa5eed0bc50cd8
masaibar <masaibar.dev@gmail.com>
2020-08-06 20:38:24 +0900

    コードは書いたもののストアにアップロードしていないので動かず

--- app/src/main/java/com/masaibar/inappreviewsample/MainActivity.kt
+++ app/src/main/java/com/masaibar/inappreviewsample/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.masaibar.inappreviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.review.ReviewManagerFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val manager = ReviewManagerFactory.create(this)
+
+        val request = manager.requestReviewFlow()
+        request.addOnCompleteListener {task ->
+            if(task.isSuccessful) {
+                val reviewInfo = task.result
+                val flow = manager.launchReviewFlow(
+                    this,
+                    reviewInfo
+                )
+                flow.addOnCompleteListener {
+
+                }
+            } else {
+                // do nothing
+            }
+        }
     }
 }
